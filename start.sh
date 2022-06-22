c0c94644261b3ff53a005ada03278d6967eb5104
『ᴘɪʀᴏ』 <96607700+ritheshrkrm17@users.noreply.github.com>
2022-06-22 22:45:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm17/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+  git clone https://github.com/ritheshrkrm17/Luci-Test-Bot.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
+  git clone $UPSTREAM_REPO /Luci-Test-Bot
 fi
 cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
